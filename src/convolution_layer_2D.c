bd405bd9fa669906e397a49697bdd161b2db2e4a
yewhan <euanhughesj@gmail.com>
2023-04-27 15:28:59 +0100

    refactored FP function names

--- src/convolution_layer_2D.c
+++ src/convolution_layer_2D.c
@@ -11,7 +11,7 @@
 // avx instructions applied
 // moved bias load outside of x to m loop
 // 10 GFLOPS, 5x speedup from unopt using -O3
-int optimised_layerv1_arraycopying_vectorised_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
+int optimised_layer_v1_AC_vectorised_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
   __m256 bias, temp, s, w;
 
   unsigned int filter_FP_length = Output_depth_dim * Mask_Y_dim * Mask_X_dim * Input_depth_dim;
@@ -103,7 +103,7 @@ int optimised_layerv1_arraycopying_vectorised_FP(const float* in_FP, const float
 
 // register blocking/ loop unroll x by factor of 2 (x+=2)
 // 18 GFLOPS, 1.8x speedup from v1
-int optimised_layerv2_unroll_x2_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
+int optimised_layer_v2_AC_unroll_x2_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
   __m256 bias, temp, temp2, s, s2, w;
 
   unsigned int filter_FP_length = Output_depth_dim * Mask_Y_dim * Mask_X_dim * Input_depth_dim;
@@ -213,7 +213,7 @@ int optimised_layerv2_unroll_x2_FP(const float* in_FP, const float* filter_FP, c
 
 // register block/ unroll x by factor of 2 again (x+=4)
 // 34 GFLOPS, 1.88x speedup from v2
-int optimised_layerv3_unroll_x4_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
+int optimised_layer_v3_AC_unroll_x4_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
   __m256 bias, temp, temp2, temp3, temp4, s, s2, s3, s4, w;
 
   unsigned int filter_FP_length = Output_depth_dim * Mask_Y_dim * Mask_X_dim * Input_depth_dim;
@@ -352,7 +352,7 @@ int optimised_layerv3_unroll_x4_FP(const float* in_FP, const float* filter_FP, c
 
 // register block/ unroll m by factor of 2 (m+=16)
 // 61 GFLOPS, 1.79x speedup from v3
-int optimised_layerv4_unroll_m16_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
+int optimised_layer_v4_AC_unroll_m16_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
   __m256 bias, bias2, temp, temp2, temp3, temp4, temp5, temp6, temp7, temp8, s, s2, s3, s4, w, w2;
 
   unsigned int filter_FP_length = Output_depth_dim * Mask_Y_dim * Mask_X_dim * Input_depth_dim;
@@ -539,7 +539,7 @@ int optimised_layerv4_unroll_m16_FP(const float* in_FP, const float* filter_FP, 
 // reduce register pressure on d loop, as v4 uses 20 general purpose registers, now uses 16
 // also strength reduction on d loop (spent less time computing values)
 // 64 GFLOPS, 1.05x speedup from v4
-int optimised_layerv5_register_pressure_d_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
+int optimised_layer_v5_AC_register_pressure_d_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
   __m256 bias, bias2, temp, temp2, temp3, temp4, temp5, temp6, temp7, temp8, s, s2, s3, s4, w, w2;
 
   unsigned int filter_FP_length = Output_depth_dim * Mask_Y_dim * Mask_X_dim * Input_depth_dim;
@@ -726,7 +726,7 @@ int optimised_layerv5_register_pressure_d_FP(const float* in_FP, const float* fi
 // reduce register pressure on x loop, as v5 uses 15, v6 uses 8 (shouldn't have huge impact)
 // also strength reduction on x
 // 64 GFLOPS, no noticable difference from v5, but worth keeping
-int optimised_layerv6_register_pressure_x_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
+int optimised_layer_v6_AC_register_pressure_x_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
   __m256 bias, bias2, temp, temp2, temp3, temp4, temp5, temp6, temp7, temp8, s, s2, s3, s4, w, w2;
 
   unsigned int filter_FP_length = Output_depth_dim * Mask_Y_dim * Mask_X_dim * Input_depth_dim;
@@ -913,7 +913,7 @@ int optimised_layerv6_register_pressure_x_FP(const float* in_FP, const float* fi
 // replace mul with shift ops in d loop
 // IN CURRENT FORM DOESN'T WORK WHEN OUT/INPUT DEPTHS ARE CHANGED
 // 63 GFLOPS, 1.02x SLOWDOWN from v6, maybe revisit later
-int optimised_layerv7_strength_reduction_d_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
+int optimised_layer_v7_AC_strength_reduction_d_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
   #define output_depth_lshift 7   // x * output_depth_dim == x << 7
   #define input_depth_lshift 8    // x * input_depth_dim == x << 8
   #define in_out_depth_lshift 15  // x * input_depth_dim * output_depth_dim == x << 15
@@ -1060,7 +1060,7 @@ int optimised_layerv7_strength_reduction_d_FP(const float* in_FP, const float* f
 
 // loop tiling on m loop
 // 65 GFLOPS, 1.02x speedup from v6 when m = 128(low), results in less cache misses so perf scales with m
-int optimised_layerv8_loop_tiling_m_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
+int optimised_layer_v8_AC_loop_tiling_m_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
   #define m_tile 16
 
   __m256 bias, bias2, temp, temp2, temp3, temp4, temp5, temp6, temp7, temp8, s, s2, s3, s4, w, w2;
@@ -1205,7 +1205,7 @@ int optimised_layerv8_loop_tiling_m_FP(const float* in_FP, const float* filter_F
 
 // unroll d loop by 2
 // 66 GFLOPS, 1.02x speedup from v8
-int optimised_layerv9_unroll_d2_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
+int optimised_layer_v9_AC_unroll_d2_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
   #define m_tile 16
 
   __m256 bias, bias2, temp, temp2, temp3, temp4, temp5, temp6, temp7, temp8, s, s2, s3, s4, w, w2;
@@ -1370,7 +1370,7 @@ int optimised_layerv9_unroll_d2_FP(const float* in_FP, const float* filter_FP, c
 
 // unroll d loop by 4
 // 65 GFLOPS, 1.02x slowdown from v9?
-int optimised_layerv10_unroll_d4_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
+int optimised_layer_v10_AC_unroll_d4_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
   #define m_tile 16
 
   __m256 bias, bias2, temp, temp2, temp3, temp4, temp5, temp6, temp7, temp8, s, s2, s3, s4, w, w2;
@@ -1453,7 +1453,7 @@ int optimised_layerv10_unroll_d4_FP(const float* in_FP, const float* filter_FP, 
                   temp5 = _mm256_fmadd_ps(s, w2, temp5);
                   temp6 = _mm256_fmadd_ps(s2, w2, temp6);
                   temp7 = _mm256_fmadd_ps(s3, w2, temp7);
-                  temp8 = _mm256_fmadd_ps(s4, w2, temp8);
+                  temp8 = _mm256_fmadd_ps(s4, w2, temp8);#
 
 
                   // __________________________________________________________________________
@@ -1581,7 +1581,7 @@ int optimised_layerv10_unroll_d4_FP(const float* in_FP, const float* filter_FP, 
 
 // unroll d loop by 8
 // 76 GFLOPS, 1.15x speedup from v9
-int optimised_layerv11_unroll_d8_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
+int optimised_layer_v11_AC_unroll_d8_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
   #define m_tile 16
 
   __m256 bias, bias2, temp, temp2, temp3, temp4, temp5, temp6, temp7, temp8, s, s2, s3, s4, w, w2;
@@ -1888,7 +1888,7 @@ int optimised_layerv11_unroll_d8_FP(const float* in_FP, const float* filter_FP, 
 
 // move operations outside of d loop, general registers used in d loop = 16
 // 81 GFLOPS, 1.07x speedup from v11
-int optimised_layerv12_ops_outside_loop_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
+int optimised_layer_v12_AC_ops_outside_loop_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
   #define m_tile 16
 
   __m256 bias, bias2, temp, temp2, temp3, temp4, temp5, temp6, temp7, temp8, s, s2, s3, s4, w, w2;
@@ -2201,7 +2201,7 @@ int optimised_layerv12_ops_outside_loop_FP(const float* in_FP, const float* filt
 
 // change ints to unsigned ints in array copying loop to prevent expensive conversion instructions
 // 82 GFLOPS, 1.01x speedup from v12, likely within margin of error due to small size of array copying loop
-int optimised_layerv13_arraycopying_sign_unsigned_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
+int optimised_layer_v13_AC_sign_unsigned_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
   #define m_tile 16
 
   __m256 bias, bias2, temp, temp2, temp3, temp4, temp5, temp6, temp7, temp8, s, s2, s3, s4, w, w2;
@@ -2514,7 +2514,7 @@ int optimised_layerv13_arraycopying_sign_unsigned_FP(const float* in_FP, const f
 
 // make use of OpenMP API, 2 parallel calls
 // 483 GFLOPS, 5.85x speedup from v13
-int optimised_layerv14_omp_2blocks_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
+int optimised_layer_v14_AC_omp_2blocks_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
   #define m_tile 16
 
   __m256 bias, bias2, temp, temp2, temp3, temp4, temp5, temp6, temp7, temp8, s, s2, s3, s4, w, w2;
@@ -2830,7 +2830,7 @@ int optimised_layerv14_omp_2blocks_FP(const float* in_FP, const float* filter_FP
 
 // make use of OpenMP API, 1 parallel call
 // 456 GFLOPS, 1.06x slowdown from v14, not expected
-int optimised_layerv15_omp_1block_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
+int optimised_layer_v15_AC_omp_1block_FP(const float* in_FP, const float* filter_FP, const float* bias_array_FP, float* out_to_compare_with_FP) {
   #define m_tile 16
 
   __m256 bias, bias2, temp, temp2, temp3, temp4, temp5, temp6, temp7, temp8, s, s2, s3, s4, w, w2;
@@ -2876,7 +2876,9 @@ int optimised_layerv15_omp_1block_FP(const float* in_FP, const float* filter_FP,
     // main loop body
     #pragma omp for private(bias, bias2, temp, temp2, temp3, temp4, temp5, temp6, temp7, temp8, s, s2, s3, s4, w, w2)\
      collapse(5) schedule(static) 
-    for (unsigned int mm = 0; mm < Output_depth_dim; mm += m_tile) {
+    for (unsigned int mm = 0; mm < Output_depth_dim; mm += m_tile) {  // loop tiling not really worth here since there should be at least 2 loops between the tiling and the tiled loop
+                                                                      // experiment with moving loops around, b,y,m / others
+                                                                      // b should be the outermost loop no matter what???
       for (unsigned int b = 0; b < Input_Output_batch_dim; b++) { //batch
         for (unsigned int m = mm; m < mm + m_tile; m+=16) { //channels
           bias = _mm256_load_ps(&bias_array_FP[m]);
@@ -3147,7 +3149,7 @@ int optimised_layerv15_omp_1block_FP(const float* in_FP, const float* filter_FP,
 }
 
 
-
+// naming convention optimised_layer_vX_AC_OptApplied_FP/Char
 
 
 
