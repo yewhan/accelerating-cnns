c07de458e4d577124dfc761250ad996b571139e1
yewhan <euanhughesj@gmail.com>
2023-04-29 23:33:23 +0100

    unrolled d loop of x3 m3 unopt by 32

--- src/main.c
+++ src/main.c
@@ -155,7 +155,8 @@ int main() {
     // optimised_layer_v3_x3m3_unroll_d16_FP(in_FP, filter_FP, bias_array_FP, out_FP);
     // optimised_layer_v3_x3m3_unroll_d16_v2_FP(in_FP, filter_FP, bias_array_FP, out_FP);
     // optimised_layer_v3_x2m4_unroll_d16_FP(in_FP, filter_FP, bias_array_FP, out_FP);
-    optimised_layer_v3_x3m3_unroll_d32_FP(in_FP, filter_FP, bias_array_FP, out_FP);
+    // optimised_layer_v3_x3m3_unroll_d32_FP(in_FP, filter_FP, bias_array_FP, out_FP);
+    optimised_layer_v3_x3m3_unroll_d32_v2_FP(in_FP, filter_FP, bias_array_FP, out_FP);
 
 
 
