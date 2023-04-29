4fcbe8d0f627c4f650b092889260d56b89364e66
yewhan <euanhughesj@gmail.com>
2023-04-29 17:01:42 +0100

    unrolled d loop for last added function

--- src/main.c
+++ src/main.c
@@ -152,6 +152,7 @@ int main() {
     // optimised_layer_v2_unroll_x2m4_hadd_register_pressure_FP(in_FP, filter_FP, bias_array_FP, out_FP);
     // optimised_layer_v2_unroll_x3m3_hadd_FP(in_FP, filter_FP, bias_array_FP, out_FP);
     optimised_layer_v2_unroll_x3m3_hadd_opt_FP(in_FP, filter_FP, bias_array_FP, out_FP);
+    // optimised_layer_v3_unroll_d16_FP(in_FP, filter_FP, bias_array_FP, out_FP);
 
 
 
